25d213293a8c07e6d220f73a266da3f1fc65d812
lara-maleen <lara-beckmann@freenet.de>
2019-07-04 12:27:27 +0200

    Update cluster_file_IBM_Lara.R

--- cluster_file_IBM_Lara.R
+++ cluster_file_IBM_Lara.R
@@ -10,10 +10,13 @@ registerDoMC(cores)
 
 setwd("/data/home/lara")
   
-  source('IBM_fur_seals_Lara_cluster.R') #load in file with the IBM model (source for simulation.fun) 
+  source('fur_seals/IBM_fur_seals_Lara_cluster.R') #load in file with the IBM model (source for simulation.fun) 
+
+#source('fur_seals/genes.rda')#load gene map
+
   
   #create new folder with todays date (change 'test' if necessary to number etc), also the working directory is changed to this new folder
-  today <- function(prefix = "Test") {
+  today <- function(prefix = "lt2") {
     newdir <- paste(prefix, Sys.Date(), sep = "_")
     dir.create(newdir)
     setwd(newdir)
@@ -21,8 +24,8 @@ setwd("/data/home/lara")
   
   today() #new folder created and wd set 
   
-  # create a list of the parameter values
-  df <- expand.grid(die.fight = seq(0,1,0.25), die = seq(0,1,0.24),  replicate = 1:3) #df contains parameter matrix + replicate number
+  # create a list of the parameter values 
+ df <- expand.grid(p=seq(0.1,0.2,0.02), replicate = 1:10) #df contains parameter matrix + replicate number
   df$filename <- paste("output",1:nrow(df),".csv",sep="") #add file names (csv files)
   set.seed(NULL)
   df$seed <- round(runif(nrow(df),0,.Machine$integer.max)) #add seed number that is fix for this run
@@ -33,16 +36,20 @@ blabla <-  foreach(i = 1:nrow(df)) %dopar% {
     set.seed(df$seed[i])
     
     #... run the simulation function with the right parameter values
-    tmp <- simulation.fun(time = 100, #t  
-                          age = 5, 
+    tmp <- simulation.fun(time = 10000, #t  
+                          age = 15, 
                           patches = 2, #number of Patches (two different sites: high/low density)
                           territories = c(50,50), #number of territories per patch
                           mutate = 0.05, #mutationfactor
-                          die = df$die[i], #mortality rate 
-                          die.fight = df$die.fight[i], #propability to die from fight
-                          loci.col = c(12:31), #in which columns of the pop matrix are the loci?
-                          i = 0.1, #intercept for infanticide function
-                          s = 0.2)
+                          #die = 0.18, #mortality rate 
+                          die.fight = 0.35, #propability to die from fight
+                          loci.col = c(14:53), #in which columns of the pop matrix are the loci?
+                          p = df$p[i],
+			  u=80,
+			  i = -0.8, #intercept for infanticide function
+                          s = 1.8,
+			  surv=0.9	
+			)
     
     write.csv(tmp, file=df$filename[i])
     return(NULL)
