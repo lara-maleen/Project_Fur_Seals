448d34be7678f9be0cf208f9e2fb367f4072e8c1
koenvanbenthem <koenvanbenthem@gmail.com>
2020-02-06 19:18:40 +0100

    update statistics fnctn

--- matrix_model.R
+++ matrix_model.R
@@ -206,51 +206,85 @@ statistics <- function(dat,dum){
   pert_ind <- 0
   max_dist <- 0
   for(i in 1:length(base_vec)){
-    pert_up <- base_vec
-    pert_down <- base_vec
-    pert_up[i] <- (1+delta)*pert_up[i]
-    pert_down[i] <- (1-delta)*pert_down[i]
-    dist_prior_up <- dist(pert_up,base_vec)
-    dist_prior_down <- dist(pert_down,base_vec)
-    
-    for(t in 1:Tp){
-      A_up <- make_mat(pert_up,dum)
-      A_down <- make_mat(pert_down,dum)
+      atzero <- round(base_vec[i],4)==0
+      pert_up <- base_vec
+      pert_down <- base_vec
       
-      pert_up <- A_up %*% pert_up
+      pert_up[i] <- pert_up[i] + delta
+      pert_down[i] <- pert_down[i] - delta
+  
+      pert_down[pert_down < 0] <- 0
+      
       pert_up <- pert_up/sum(pert_up)
-      pert_down <- A_down %*% pert_down
       pert_down <- pert_down/sum(pert_down)
-    #   if(i == 8){
-    #     pert_up <- 0.0*pert_up + 1.0*rep(1/length(base_vec),length(base_vec))
-    #   }
-    #   
-    #   if(i == 9){
-    #     pert_down <- 0.9*pert_down + 0.1*rep(1/length(base_vec),length(base_vec))
-    #   }
-    # }
-    dist_post_up <- dist(pert_up,base_vec)
-    dist_post_down <- dist(pert_down,base_vec)
-    
-    up_pert <- dist_post_up - dist_prior_up
-    down_pert <- dist_post_down - dist_prior_down
-    
-    if((up_pert < 0 | down_pert < 0) & max(abs(c(up_pert,down_pert))) > max_dist){
-      pert_ind <- i*(-1+2*as.numeric(abs(up_pert) > abs(down_pert)))
-      max_dist <- abs(max(c(up_pert,down_pert)))
       
-    }
+      dist_prior_up <- dist(pert_up,base_vec)
+      dist_prior_down <- dist(pert_down,base_vec)
+      
+      for(t in 1:Tp){
+        A_up <- make_mat(pert_up,dum)
+        pert_up <- A_up %*% pert_up
+        pert_up <- pert_up/sum(pert_up)
+        
+        if(!atzero){
+          A_down <- make_mat(pert_down,dum)
+          pert_down <- A_down %*% pert_down
+          pert_down <- pert_down/sum(pert_down)
+        }
+        
+        if(i == 8){
+            pert_up <- 0.9*pert_up + 0.1*rep(1/length(base_vec),length(base_vec))
+        }
+
+        if(i == 6){
+            pert_down <- 0.0*pert_down + 1.0*rep(1/length(base_vec),length(base_vec))
+        }
+
+      }
+        
+      dist_post_up <- dist(pert_up,base_vec)
+      dist_post_down <- dist(pert_down,base_vec)
+      
+      cat(dist_post_up,"\t",dist_prior_up,"\n")
+      
+      up_pert <- dist_post_up - dist_prior_up # negative values indicate that the distance has decreased over time
+                                              # and that the system is stable.
+                                              # positive values indicate an system that's still moving forward.
+                                              # more positive values mean larger differences over time
+      down_pert <- dist_post_down - dist_prior_down
+
+      
+      if(up_pert > 0 & up_pert > max_dist){
+        pert_ind <- i
+        max_dist <- up_pert
+      }
+      
+      if(!atzero & down_pert > 0 & down_pert > max_dist){
+        pert_ind <- -i
+        max_dist <- down_pert
+      }
+    
   }
-   return(pert_ind)
+  
   
   # differences in abundances between the islands
   
+  sumdat <- dum
+  
+  # add columns for island 1 and 2
+  sumdat$N.1 <- sumdat$p1*base_vec
+  sumdat$N.2 <- (1-sumdat$p1)*base_vec
+  
+  # summarize these numbers in relevant statistics
+  
   # differences in allele frequencies
+    
+  return(list(stability = pert_ind,sumdat))
 }
-statistics(normalized,dum2)
+tmp <- statistics(normalized,dum2)
 
-plotting <- function(){
+plotting <- function(stats){
   return(NA)
 }
 
-plotting()
+plotting(tmp)
